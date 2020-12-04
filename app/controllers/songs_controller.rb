a6592df66a26ab6a0613d3b7dc6b0de8904593ee
Raveen K Sharma <raveen08@gmail.com>
2020-12-04 20:37:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,11 +25,8 @@ class SongsController < ApplicationController
   end
 
   def new
-    @preference = Preference.first
-    if @preference.allow_create_artists
-       @artist = Artist.new
-    else
-      redirect_to artists_path
+    
+
     end
   end
 
