985b84f117c87e55498f697bb4f82311c36a1fc3
Raveen K Sharma <raveen08@gmail.com>
2020-12-04 20:07:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
     resources :songs, only: [:index, :show]
   end
   resources :songs
+  
+  
+  namespace :preference do
+    resources :stats, only: [:index]
+  end
+
 
 end
